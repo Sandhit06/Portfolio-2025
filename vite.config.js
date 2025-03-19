410c1100fb72087f9e9016676c021116ee12ce40
Sandhit Karmakar <sandhitkarmakar@gmail.com>
2025-03-19 12:15:27 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  optimizeDeps: {
-    include: ["react", "react-dom"], // Ensures React is bundled first
-  },
-  build: {
-    chunkSizeWarningLimit: 2000,
-    minify: false, // Avoid warnings for large chunks
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("@mui")) return "mui-vendor";
-            if (id.includes("three")) return "three-vendor";
-            return "vendor"; // General vendor chunk
-          }
-        },
-      },
+  resolve: {
+    alias: {
+      react: require.resolve("react"),
+      "react/jsx-runtime": require.resolve("react/jsx-runtime"),
     },
   },
-  esbuild: {
-    drop: ["console", "debugger"], // Removes console logs in production
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
   },
 });
