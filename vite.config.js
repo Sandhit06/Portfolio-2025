0a10218aafdce38cbda37ed4aad4080fecd2b3c1
Sandhit Karmakar <sandhitkarmakar@gmail.com>
2025-03-19 12:09:09 +0530

    changes added

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react", "react-dom"], // Ensures React is bundled first
+  },
   build: {
     chunkSizeWarningLimit: 2000,
     minify: false, // Avoid warnings for large chunks
     rollupOptions: {
+      external: ["react", "react-dom"],
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
